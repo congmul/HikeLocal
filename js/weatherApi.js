27b631586d9de25787ebe9faccec734be1324696
Jehyun <congmul@hotmail.com>
2020-10-15 01:14:39 -0700

    Separating Js files depending on API

---
+++ js/weatherApi.js
@@ -0,0 +1,18 @@
+weatherDisplay(47.6062, -122.3321);
+
+
+function weatherDisplay(latitude, longitude) {
+
+    var apiKey = "55b9b01153577ab02bdcfe93626df0e5";
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely&units=imperial&appid=" + apiKey;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+
+}
