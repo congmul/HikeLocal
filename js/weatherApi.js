a897fb4bc61231483ea42d6da0c5f3f1475487a7
Ben Robinson <robinson.dri@gmail.com>
2020-10-17 11:12:43 -0700

    fixing bugs

--- js/weatherApi.js
+++ js/weatherApi.js
@@ -3,31 +3,8 @@ const queryWeather = async (latitude, longitude) => {
 
     let queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely&units=imperial&appid=" + apiKey;
 
-<<<<<<< HEAD
-
-function weatherDisplay(latitude, longitude) {
-
-    var apiKey = "55b9b01153577ab02bdcfe93626df0e5";
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely&units=imperial&appid=" + apiKey;
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-
-
-    
-}
-
-});
-=======
     // send asynchronous request
     const request = await fetch(queryURL);
     const data = await request.json();
     return data;
 }
->>>>>>> develop
