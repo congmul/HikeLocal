eba9e30fac5c32f1aa3cb5dd01874566b0413af0
Jehyun <congmul@hotmail.com>
2021-03-18 21:33:03 -0700

    Build server

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,8 @@
+// require('dotenv').config()
+
 $(document).ready(function () {
 
+    
     // Declare Global variables
     var address = "";
     var trailRating = 0;
@@ -31,7 +34,9 @@ $(document).ready(function () {
         $("#loading-page").css("display", "block");
 
         // Geolocation API transfer address to lat/lng
-        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyCPgJjyXg3QM3W6eJlHAtQGhct26ZY8pEI";
+        // Move to Backend
+        console.log(process.env.GOOGLE_API);
+        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + `&key=${process.env.GOOGLE_API}`;
         $.ajax({
             url: queryURL,
             method: "GET"
