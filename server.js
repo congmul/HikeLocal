5c61e865b7ab426b7f06aa1bb066cdc44a8975ec
Jehyun <congmul@hotmail.com>
2021-03-23 16:02:13 -0700

    Add signin html

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/signin.html'));
+});
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
